4b8bc77d063d94244cdd68ac408d8bf5da791168
16022653 <16022653@myrp.edu.sg>
2018-05-07 14:30:45 +0800

    lv grade

--- app/src/main/java/com/example/a16022706/p03_classjournal/Activity1.java
+++ app/src/main/java/com/example/a16022706/p03_classjournal/Activity1.java
@@ -1,35 +1,49 @@
 package com.example.a16022706.p03_classjournal;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class Activity1 extends AppCompatActivity {
+    int requestCodeForAdd = 1;
 
-    Button infoBtn;
-
+    ListView lv;
+    Button btnInfo;
+    Button btnAdd;
+    Button btnEmail;
+    ArrayAdapter aa;
+    ArrayList<DailyCA> grade;
+    String module;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_1);
-        infoBtn = (Button) findViewById(R.id.buttonInfo);
-        infoBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View arg0) {
-                // Intent to display data
-                Intent rpIntent = new Intent(Intent.ACTION_VIEW);
-                // Set the URL to be used.
-                rpIntent.setData(Uri.parse("http://www.rp.edu.sg"));
-                startActivity(rpIntent);
-            }
-        });
 
+        grade = new ArrayList<DailyCA>();
+        lv = (ListView) this.findViewById(R.id.listview);
+        btnInfo = (Button) findViewById(R.id.buttonInfo);
+        btnAdd = (Button) findViewById(R.id.buttonAdd);
+        btnEmail = (Button) findViewById(R.id.btnEmail);
+
+        Intent i = getIntent();
+        String code = i.getStringExtra("code");
+        getSupportActionBar().setTitle("Info for " + code);
+        module = code;
+
+        if (code.equalsIgnoreCase("C347")) {
+            grade.add(new DailyCA(1, "B"));
+            grade.add(new DailyCA(2, "C"));
+            grade.add(new DailyCA(3, "A"));
+        } else if (code.equalsIgnoreCase("C302")) {
+            grade.add(new DailyCA(1, "E"));
+        }
+        aa = new GradeAdapter(this, R.layout.activity_row, grade);
+        lv.setAdapter(aa);
     }
-
-
-
 }
