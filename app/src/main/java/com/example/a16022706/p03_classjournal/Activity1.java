e232088e6f4b5e4a49dcb3e5050930343d12495d
16022706 <16022706@myrp.edu.sg>
2018-05-07 12:43:01 +0800

    activity1

--- app/src/main/java/com/example/a16022706/p03_classjournal/Activity1.java
+++ app/src/main/java/com/example/a16022706/p03_classjournal/Activity1.java
@@ -1,8 +1,34 @@
 package com.example.a16022706.p03_classjournal;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class Activity1 extends AppCompatActivity {
+
+    Button infoBtn;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_1);
+        infoBtn = (Button) findViewById(R.id.buttonInfo);
+        infoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                // Intent to display data
+                Intent rpIntent = new Intent(Intent.ACTION_VIEW);
+                // Set the URL to be used.
+                rpIntent.setData(Uri.parse("http://www.rp.edu.sg"));
+                startActivity(rpIntent);
+            }
+        });
+
+    }
 
 
 
