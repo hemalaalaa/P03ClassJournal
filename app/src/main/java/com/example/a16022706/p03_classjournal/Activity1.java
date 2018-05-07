1db2c63344b08a1efd8c4334e8aeb681a93ce2e3
16022653 <16022653@myrp.edu.sg>
2018-05-07 15:23:27 +0800

    email

--- app/src/main/java/com/example/a16022706/p03_classjournal/Activity1.java
+++ app/src/main/java/com/example/a16022706/p03_classjournal/Activity1.java
@@ -1,8 +1,10 @@
 package com.example.a16022706.p03_classjournal;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
@@ -45,5 +47,37 @@ public class Activity1 extends AppCompatActivity {
         }
         aa = new GradeAdapter(this, R.layout.activity_row, grade);
         lv.setAdapter(aa);
+
+        btnEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // The action you want this intent to do;
+                // ACTION_SEND is used to indicate sending text
+                Intent email = new Intent(Intent.ACTION_SEND);
+                // Put essentials like email address, subject & body text
+                email.putExtra(Intent.EXTRA_EMAIL,new String[]{"jason_lim@rp.edu.sg"});
+                email.putExtra(Intent.EXTRA_SUBJECT,"My remarks");
+                String inform = "Hi faci,\n";
+                inform += "I am...\n Please see my remarks so far, thank you!\n";
+                for (int i = 0; i < grade.size(); i++) {
+                    String week = String.valueOf(grade.get(i).getWeek());
+                    String grades = grade.get(i).getDgGrade();
+                    inform += "Week " + week + ": DG: " + grades + "\n";
+                }
+                email.putExtra(Intent.EXTRA_TEXT,
+                        inform);
+
+                email.setType("message/rfc822");
+
+                // createChooser shows user a list of app that can handle
+                // this MIME type, which is, email
+                startActivity(Intent.createChooser(email,
+                        "Choose an Email client :"));
+
+
+            }
+        });
+
+
     }
 }
